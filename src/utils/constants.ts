936836b36065f1386082952b9247b86eb8c7526a
Danil <danil_karashchuk@mail.ru>
2024-11-26 17:09:07 +0300

    На проверку

---
+++ src/utils/constants.ts
@@ -0,0 +1,8 @@
+export const BURGER_CONSTRUCTOR_SLICE_NAME = 'burgerConstructor';
+export const FEED_SLICE_NAME = 'feed';
+export const INGREDIENTS_SLICE_NAME = 'ingredients';
+export const ORDER_SLICE_NAME = 'order';
+export const ORDERS_SLICE_NAME = 'orders';
+export const USER_SLICE_NAME = 'user';
+
+export const DATA_CY_CONSTRUCTOR = 'data-cy=constructor';
